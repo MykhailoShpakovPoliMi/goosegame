e3454cf45db54dbf63af4c6c47b5685d8372f3d6
Mykhailo Shpakov <mykhailo.shpakov@mail.polimi.it>
2023-05-26 23:58:09 +0200

    Created the project

---
+++ src/main/java/com/nttdata/it/goosegame/GoosegameApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.it.goosegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoosegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoosegameApplication.class, args);
+	}
+
+}
